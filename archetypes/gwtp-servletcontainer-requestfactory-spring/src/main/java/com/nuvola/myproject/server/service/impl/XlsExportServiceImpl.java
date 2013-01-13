2399b6bd8bcbbc4632b989985d9c050ae556e755
Kecha mohamed <Kecha.mohamed@gmail.com>
2013-01-13 17:55:00 +0000

    XLS Export Service and unit test

---
+++ archetypes/gwtp-servletcontainer-requestfactory-spring/src/main/java/com/nuvola/myproject/server/service/impl/XlsExportServiceImpl.java
@@ -0,0 +1,9 @@
+package com.nuvola.myproject.server.service.impl;
+
+import com.nuvola.myproject.server.service.XlsExportService;
+import org.adorsys.xlseasy.impl.proc.SpreadsheetServiceImpl;
+import org.springframework.stereotype.Service;
+
+@Service
+public class XlsExportServiceImpl extends SpreadsheetServiceImpl implements XlsExportService {
+}
